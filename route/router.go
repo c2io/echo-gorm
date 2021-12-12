09b0ebdf6882ff41aed64a629eb130e9133405b6
c2io <c2io@outlook.com>
2021-12-13 00:19:30 +0800

    feat: middleware log+recover

--- route/router.go
+++ route/router.go
@@ -4,10 +4,15 @@ import (
 	"echo-gorm/api"
 
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func Init() *echo.Echo {
 	e := echo.New()
+
+	e.Use(middleware.Logger())    // log request info
+	e.Use(middleware.Recover())   // auto recover from any panic
+	e.Use(middleware.RequestID()) // log request info with id
 
 	e.GET("/", api.Home)
 
